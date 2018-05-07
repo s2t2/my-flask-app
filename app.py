f20bb0523b501c0d0917d02c659842bbb61bc762
MJ Rossetti <s2t2@users.noreply.github.com>
2018-05-06 22:02:05 -0400

    Views (#1)

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask import render_template
+
+#from dotenv import load_dotenv
+#load_dotenv()
+
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
 
-def hello_world():
-    return 'Hello World'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
